f1c0a1cae9890d38b426ecc4012d7de0dd0ec925
Tuag <igortuag@gmail.com>
2022-05-07 09:24:00 -0300

    add default size to anchor

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -33,7 +33,7 @@ const GlobalStyles = createGlobalStyle`
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
   }
 
-  p {
+  p, a {
     font-size: 2rem;
     line-height: var(--medium);
   }
