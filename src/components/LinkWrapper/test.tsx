6e6bf688c14ef64b8824ebce12db1bc6641ff0e6
Tuag <igortuag@gmail.com>
2022-05-10 08:00:44 -0300

    test using role

--- src/components/LinkWrapper/test.tsx
+++ src/components/LinkWrapper/test.tsx
@@ -6,7 +6,7 @@ describe('<LinkWrapper />', () => {
   it('should render link and children', () => {
     render(<LinkWrapper href="/my-link">Anything</LinkWrapper>)
 
-    const children = screen.getByText(/anything/i)
+    const children = screen.getByRole('link', { name: /anything/i })
 
     expect(children).toBeInTheDocument()
     expect(children).toHaveAttribute('href', '/my-link')
