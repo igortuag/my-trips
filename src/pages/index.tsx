3dc18f3762602765147ac865adf20e78ab32f16b
Tuag <igortuag@gmail.com>
2022-05-11 07:38:38 -0300

    add link to page about

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,28 @@
 import dynamic from 'next/dynamic'
+import { InfoOutline } from '@styled-icons/evaicons-outline/InfoOutline'
+import LinkWrapper from 'components/LinkWrapper'
 
 const Map = dynamic(() => import('components/Map'), { ssr: false })
 
 export default function Home() {
-  return <Map
-    places={[
-      {
-        id: '1',
-        name: 'Island',
-        slug: 'island',
-        location: { 
-          latitude: 65,
-          longitude: -18
-        } 
-      }
-    ]}
-  />
+  return (
+    <>
+      <LinkWrapper href="/about">
+        <InfoOutline />
+      </LinkWrapper>
+      <Map
+        places={[
+          {
+            id: '1',
+            name: 'Island',
+            slug: 'island',
+            location: {
+              latitude: 65,
+              longitude: -18
+            }
+          }
+        ]}
+      />
+    </>
+  )
 }
