8ff96359f74fefeafdb0037336f4c0b5d43ff4af
Tuag <igortuag@gmail.com>
2022-05-11 07:45:20 -0300

    add content

--- src/templates/About/index.tsx
+++ src/templates/About/index.tsx
@@ -1,8 +1,21 @@
+import { CloseOutline } from '@styled-icons/evaicons-outline'
+import LinkWrapper from 'components/LinkWrapper'
 import * as S from './styles'
 
 const AboutTemplate = () => (
   <S.Content>
-    <h1>About</h1>
+    <LinkWrapper href="/">
+      <CloseOutline size={32} />
+    </LinkWrapper>
+
+    <S.Heading>My Trips</S.Heading>
+
+    <S.Body>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+      ratione, amet, eum quo repellat quibusdam accusamus at soluta voluptatem,
+      esse vero nihil enim doloribus. Eligendi, perspiciatis? Dignissimos veniam
+      aliquam omnis!
+    </S.Body>
   </S.Content>
 )
 
--- src/templates/About/styles.ts
+++ src/templates/About/styles.ts
@@ -1,3 +1,7 @@
 import styled from 'styled-components'
 
 export const Content = styled.div``
+
+export const Heading = styled.div``
+
+export const Body = styled.div``
