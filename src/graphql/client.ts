a1900ac25bcfe0d7a399fa03d6c2e71ec0a496b0
Tuag <igortuag@gmail.com>
2022-05-12 08:09:23 -0300

    export client

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,9 +1,11 @@
 import { GraphQLClient } from 'graphql-request'
 
-const endpoint = process.env.NEXT_PUBLIC_API_URL as string
+const endpoint = process.env.NEXT_PUBLIC_API_URL || ''
 
 const client = new GraphQLClient(endpoint, {
   headers: {
     authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`
   }
 })
+
+export default client
