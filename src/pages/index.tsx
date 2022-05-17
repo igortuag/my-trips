2912b28e70834eed2f5ec2853bf71172314da2e1
Tuag <igortuag@gmail.com>
2022-05-17 07:00:43 -0300

    add getStatic props to get places

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,20 @@
 import { MapProps } from 'components/Map'
+import client from 'graphql/client'
+import { GetPlacesQuery } from 'graphql/generated/graphql'
+import { GET_PLACES } from 'graphql/queries'
+import { GetStaticProps } from 'next'
 import HomeTemplate from 'templates/Home'
 
 export default function Home({ places }: MapProps) {
   return <HomeTemplate places={places} />
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { places } = await client.request<GetPlacesQuery>(GET_PLACES)
+
+  return {
+    props: {
+      places
+    }
+  }
 }
