e39dd7c0703b9215cf00b56005eb6c6b7dfa616d
Tuag <igortuag@gmail.com>
2022-05-21 06:55:01 -0300

    add config to isFallback

--- src/templates/Place/index.tsx
+++ src/templates/Place/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { CloseOutline } from '@styled-icons/evaicons-outline'
 import LinkWrapper from 'components/LinkWrapper'
 import * as S from './styles'
+import { useRouter } from 'next/router'
 
 type ImageProps = {
   url: string
@@ -21,34 +22,40 @@ export type PlaceTemplateProps = {
   }
 }
 
-const PlaceTemplate = ({ place }: PlaceTemplateProps) => (
-  <>
-    <LinkWrapper href="/">
-      <CloseOutline aria-label="Go back to map" size={32} />
-    </LinkWrapper>
+const PlaceTemplate = ({ place }: PlaceTemplateProps) => {
+  const { isFallback } = useRouter()
 
-    <S.Wrapper>
-      <S.Container>
-        <S.Heading>{place.name}</S.Heading>
-        <S.Body
-          dangerouslySetInnerHTML={{
-            __html: place.description.html
-          }}
-        />
-        <S.Gallery>
-          {place.gallery.map(({ url, height, width }, index) => (
-            <Image
-              key={index}
-              src={url}
-              height={height}
-              width={width}
-              quality={75}
-            />
-          ))}
-        </S.Gallery>
-      </S.Container>
-    </S.Wrapper>
-  </>
-)
+  if (isFallback) return null
+
+  return (
+    <>
+      <LinkWrapper href="/">
+        <CloseOutline aria-label="Go back to map" size={32} />
+      </LinkWrapper>
+
+      <S.Wrapper>
+        <S.Container>
+          <S.Heading>{place.name}</S.Heading>
+          <S.Body
+            dangerouslySetInnerHTML={{
+              __html: place.description.html
+            }}
+          />
+          <S.Gallery>
+            {place.gallery.map(({ url, height, width }, index) => (
+              <Image
+                key={index}
+                src={url}
+                height={height}
+                width={width}
+                quality={75}
+              />
+            ))}
+          </S.Gallery>
+        </S.Container>
+      </S.Wrapper>
+    </>
+  )
+}
 
 export default PlaceTemplate
