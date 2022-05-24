05175316eaf6469011f4e9c0980f6a0862bbc6ef
Tuag <igortuag@gmail.com>
2022-05-24 07:03:50 -0300

    add default seo config

---
+++ next-seo.config.js
@@ -0,0 +1,13 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://www.my-trips.igortuag.com',
+    site_name: 'My Trips'
+  },
+  twitter: {
+    handle: '@igortuag',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
