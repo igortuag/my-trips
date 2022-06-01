cffe34bd603341f658a436b82e1a754e76fbb269
Tuag <igortuag@gmail.com>
2022-06-01 06:48:02 -0300

    add setmin zoom to 2 on mobile usign mapconsumer

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,4 +1,4 @@
-import { MapContainer, TileLayer, Marker } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, MapConsumer } from 'react-leaflet'
 import { useRouter } from 'next/router'
 
 import * as S from './styles'
@@ -51,6 +51,20 @@ const Map = ({ places }: MapProps) => {
         ]}
         // worldCopyJump={true}
       >
+        <MapConsumer>
+          {(map) => {
+            const width =
+              window.innerWidth ||
+              document.documentElement.clientWidth ||
+              document.body.clientWidth
+
+            if (width < 768) {
+              map.setMinZoom(2)
+            }
+
+            return null
+          }}
+        </MapConsumer>
         <CustomTileLayer />
 
         {places?.map(({ id, slug, name, location }) => {
