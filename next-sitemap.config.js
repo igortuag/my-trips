ce4427e66b1129059a67015c7cbae79fece23b62
Tuag <igortuag@gmail.com>
2022-06-05 08:48:40 -0300

    generate sitemap to project

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,5 @@
-/** @type {import('next-sitemap').IConfig} */
-const config = {
+module.exports = {
   siteUrl: 'https://my-trips.igortuag.com',
   generateRobotsTxt: true,
   sitemapSize: 7000
 }
-
-export default config
