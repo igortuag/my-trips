0baf4d2e71e1424d860eeeaab49db4f3b9ddff29
Tuag <igortuag@gmail.com>
2022-06-05 08:41:26 -0300

    add next-sitemap custom config

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://my-trips.igortuag.com',
+  generateRobotsTxt: true,
+  sitemapSize: 7000
+}
+
+export default config
